440146acd111bc2858e7031af9dcfd43038cfbcc
Shorif4321 <shorifali4321@gmail.com>
2025-10-26 23:46:00 +0600

    admin

--- index.js
+++ index.js
@@ -62,6 +62,16 @@ async function run() {
         })
 
 
+        // admin get/checking
+        app.get("/users/admin/:email",async(req,res)=>{
+            const email = req.params.email;
+            const query = {email};
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin: user?.role === 'admin'})
+            
+        })
+
+
     } finally {
         // await client.close();
     }
